24353bd22dff0159e021d0bb443aef9dbfc94178
BuildTools <j26walker@gmail.com>
2016-04-05 20:25:37 -0400

    Fix blocks shaping to array

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ var a = [
               [1,0,1,1,1],
             ];
 
-function growth(){
+function makeMap(){
+  $("#holder").css("width", a[0].length*50+a[0].length+5);
+  $("#holder").css("height", a.length*50+a.length+5);
   for(var r = 0; r < a.length; r++){
     var aa = a[r];
     for(var c = 0; c < aa.length; c++){
       if(a[r][c] === 1){
-        console.log(1);
-        $(".holder").append('<div id ="red" class="inline"></div>');
+        $("#holder").append('<div id ="red"></div>');
       } else if (a[r][c] === 0) {
-        console.log(0);
-        $(".holder").append('<div id="grey" class="inline"></div>');
+        $("#holder").append('<div id="grey"></div>');
       }
     }
   }
 }
 
 $(document).ready(function(){
-  growth();
+  makeMap();
 });
